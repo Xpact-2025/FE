14d65cc46caadb0381c5cd6ae6abb1293b9f6891
Eunjin Cho <whdmswls9207@gmail.com>
2025-04-03 02:01:29 +0900

    feat/#12 : type system

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { getTest } from '@/apis/test';
 
 const DashboardPage = async () => {
   const test = await getTest();
-  return <div className="text-primary">{test.message}</div>;
+  return <div className="text-primary headline-60-m">{test.message}</div>;
 };
 
 export default DashboardPage;
