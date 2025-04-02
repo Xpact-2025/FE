3692b9768b03c88fc225eeb9859ceadfa3875eb6
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-04-02 18:20:59 +0900

    feat/#11 : 경험 생성 인증 로직 추가(auth.ts)

--- apis/auth.ts
+++ apis/auth.ts
@@ -32,6 +32,38 @@ interface SignupResponse {
   };
 }
 
+interface ExperiencePayload {
+  status: 'DRAFT' | 'SAVE';
+  experienceType:
+    | 'INTERN'
+    | 'EXTERNAL_ACTIVITIES'
+    | 'CONTEST'
+    | 'PROJECT'
+    | 'CERTIFICATES'
+    | 'ACADEMIC_CLUB'
+    | 'EDUCATION'
+    | 'PRIZE'
+    | 'VOLUNTEER_WORK'
+    | 'STUDY_ABROAD'
+    | 'ETC';
+  formType: 'STAR_FORM' | 'SIMPLE_FORM';
+  title: string;
+  startDate: Date;
+  endDate: Date;
+  keyword: string;
+  situation?: string;
+  task?: string;
+  action?: string;
+  result?: string;
+  role?: string;
+  perform?: string;
+}
+
+interface ExperienceResponse {
+  httpStatus: number;
+  message: string;
+}
+
 export async function loginUser(payload: LoginPayload): Promise<LoginResponse> {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
     method: 'POST',
@@ -45,7 +77,9 @@ export async function loginUser(payload: LoginPayload): Promise<LoginResponse> {
   return data;
 }
 
-export async function signupUser(payload: SignupPayload): Promise<SignupResponse> {
+export async function signupUser(
+  payload: SignupPayload
+): Promise<SignupResponse> {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/signup`, {
     method: 'POST',
     headers: {
@@ -56,6 +90,21 @@ export async function signupUser(payload: SignupPayload): Promise<SignupResponse
       type: payload.type ?? 'FORM',
       role: payload.role ?? 'ROLE_USER',
     }),
+  });
+
+  const data = await res.json();
+  return data;
+}
+
+export async function saveExperience(
+  payload: ExperiencePayload
+): Promise<ExperienceResponse> {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/exp`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
   });
 
   const data = await res.json();
