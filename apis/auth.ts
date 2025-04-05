9f698127f0cce2277707a655375fee8aa476b528
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-04-05 12:04:03 +0900

    feat/#11: 경험 api 수정(exp.ts)

--- apis/auth.ts
+++ apis/auth.ts
@@ -32,38 +32,6 @@ interface SignupResponse {
   };
 }
 
-interface ExperiencePayload {
-  status: 'DRAFT' | 'SAVE';
-  experienceType:
-    | 'INTERN'
-    | 'EXTERNAL_ACTIVITIES'
-    | 'CONTEST'
-    | 'PROJECT'
-    | 'CERTIFICATES'
-    | 'ACADEMIC_CLUB'
-    | 'EDUCATION'
-    | 'PRIZE'
-    | 'VOLUNTEER_WORK'
-    | 'STUDY_ABROAD'
-    | 'ETC';
-  formType: 'STAR_FORM' | 'SIMPLE_FORM';
-  title: string;
-  startDate: Date;
-  endDate: Date;
-  keyword: string;
-  situation?: string;
-  task?: string;
-  action?: string;
-  result?: string;
-  role?: string;
-  perform?: string;
-}
-
-interface ExperienceResponse {
-  httpStatus: number;
-  message: string;
-}
-
 export async function loginUser(payload: LoginPayload): Promise<LoginResponse> {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/auth/login/form`,
@@ -93,21 +61,6 @@ export async function signupUser(
       type: payload.type ?? 'FORM',
       role: payload.role ?? 'ROLE_USER',
     }),
-  });
-
-  const data = await res.json();
-  return data;
-}
-
-export async function saveExperience(
-  payload: ExperiencePayload
-): Promise<ExperienceResponse> {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/exp`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
   });
 
   const data = await res.json();
---
+++ apis/exp.ts
@@ -0,0 +1,46 @@
+interface ExperiencePayload {
+  status: 'DRAFT' | 'SAVE';
+  experienceType:
+    | 'INTERN'
+    | 'EXTERNAL_ACTIVITIES'
+    | 'CONTEST'
+    | 'PROJECT'
+    | 'CERTIFICATES'
+    | 'ACADEMIC_CLUB'
+    | 'EDUCATION'
+    | 'PRIZE'
+    | 'VOLUNTEER_WORK'
+    | 'STUDY_ABROAD'
+    | 'ETC';
+  formType: 'STAR_FORM' | 'SIMPLE_FORM';
+  title: string;
+  startDate: Date;
+  endDate: Date;
+  keyword: string;
+  situation?: string;
+  task?: string;
+  action?: string;
+  result?: string;
+  role?: string;
+  perform?: string;
+}
+
+interface ExperienceResponse {
+  httpStatus: number;
+  message: string;
+}
+
+export async function saveExperience(
+  payload: ExperiencePayload
+): Promise<ExperienceResponse> {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/exp`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+
+  const data = await res.json();
+  return data;
+}
