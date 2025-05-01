949a3d114d7ecd714c280393929f4bc761076523
Eunjin Cho <whdmswls9207@gmail.com>
2025-05-01 22:56:43 +0900

    feat/#42: 경험 유형에 따라 색상 변경

--- app/(main)/experience/components/ExpType.tsx
+++ app/(main)/experience/components/ExpType.tsx
@@ -1,10 +1,15 @@
 import { EXPERIENCE_OPTIONS } from '@/constants/experienceOptions';
+import { EXPERIENCE_STYLES } from '@/constants/experienceStyles';
 import { ExperienceType } from '@/types/exp';
 
 export default function ExpType({ expType }: { expType: ExperienceType }) {
   const label = EXPERIENCE_OPTIONS[expType]?.label || expType;
+  const expTypeStyle = EXPERIENCE_STYLES[expType] || EXPERIENCE_STYLES.ETC;
+
   return (
-    <div className="body-14-sb py-1 text-center font-semibold rounded-full text-pink-50 bg-pink-50-20 w-[79px] h-[24px] flex items-center justify-center">
+    <div
+      className={`body-14-sb py-1 text-center font-semibold rounded-full ${expTypeStyle} w-[79px] h-[24px] flex items-center justify-center`}
+    >
       {label}
     </div>
   );
