de608f1a63b97255dc6c61a7b94e7955d6cbc45f
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-05-03 02:16:47 +0900

    feat/#43: TopBar component

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -1,3 +1,19 @@
+'use client';
+
+import SearchBar from './SearchBar';
+import Profile from './Profile';
+import { usePathname } from 'next/navigation';
+
 export default function TopBar() {
-  return <header className="bg-[#020202] h-[100px]"></header>;
+  const pathname = usePathname();
+  const pageState = pathname === '/experience' ? 'main' : 'more';
+
+  return (
+    <div className="flex justify-between pl-[398px] pr-[130px] bg-gray-900 h-[100px] p-7">
+      {pageState === 'main' && <SearchBar />}
+      <div className="flex justify-end flex-grow">
+        <Profile />
+      </div>
+    </div>
+  );
 }
