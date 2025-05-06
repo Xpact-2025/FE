49d6c64fa092a00bc977e339bded4093ca9a0d70
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-05-06 18:33:53 +0900

    feat/#52: BtnExpType component 생성

---
+++ app/components/BtnExpType.tsx
@@ -0,0 +1,24 @@
+import { MouseEventHandler } from 'react';
+
+interface BtnExpTypeProps {
+  label: string;
+  selected: boolean;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function BtnExpType({
+  label,
+  selected,
+  onClick,
+}: BtnExpTypeProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`w-[120px] h-11 px-12 py-2.5 gap-[7px] rounded-lg border items-center justify-center inline-flex whitespace-nowrap
+        ${selected ? 'bg-primary-50 text-gray-800 text-lg font-medium' : 'bg-gray-800 text-gray-300 border-gray-50-20 text-lg font-medium'}`}
+    >
+      {label}
+    </button>
+  );
+}
