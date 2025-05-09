272c5f37118ab1f4a08a7d5a7815e5646d0a1607
Eunjin Cho <whdmswls9207@gmail.com>
2025-05-09 17:41:00 +0900

    feat/#59: popup 컴포넌트 디자인 시스템 적용

--- app/components/Popup.tsx
+++ app/components/Popup.tsx
@@ -19,28 +19,26 @@ export default function Popup({
 }: PopupProps) {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-      <div className="w-[464px] h-72 bg-gray-700 rounded-lg outline outline-gray-600">
+      <div className="w-[464px] h-[294px] bg-gray-700 rounded-[19.5px] outline outline-gray-600">
         <div className="flex flex-col items-center justify-center p-6">
           <WarningIcon />
-          <div className="text-gray-50 text-2xl font-semibold pt-6">
-            {title}
-          </div>
-          <div className="text-gray-200 text-lg text-center whitespace-pre-line pt-3">
+          <div className="text-gray-50 body-26-sb py-6">{title}</div>
+          <div className="text-gray-300 body-18-r text-center whitespace-pre-line">
             {content}
           </div>
         </div>
-        <div className="flex justify-center gap-2.5">
+        <div className="flex justify-center gap-2.5 text-center text-gray-50 body-20-sb">
           <button
             type="button"
             onClick={onCancel}
-            className="w-40 h-14 bg-gray-600 rounded-[130px] text-center text-gray-50 body-20-sb"
+            className="w-40 h-14 bg-gray-600 rounded-[130px]"
           >
             {cancelText}
           </button>
           <button
             type="button"
             onClick={onConfirm}
-            className="w-40 h-14 bg-primary-50 rounded-[130px] text-center text-gray-50 body-20-sb"
+            className="w-40 h-14 bg-primary-50 rounded-[130px]"
           >
             {confirmText}
           </button>
