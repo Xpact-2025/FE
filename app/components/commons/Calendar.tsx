d2f86292903ad6bc6f632c5390d3804a09e10283
Eunjin Cho <whdmswls9207@gmail.com>
2025-05-13 19:19:57 +0900

    feat/#61: 버튼 아이콘 svg 적용:

--- app/components/commons/Calendar.tsx
+++ app/components/commons/Calendar.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { useCalendar } from '@/hooks/useCalendar';
 import { WEEK_DAYS } from '../../../constants/calendar';
+import BackIcon from '@/public/icons/Chevron_Left.svg';
 
 interface CalendarProps {
   markedDates?: Record<string, number>;
@@ -26,20 +27,24 @@ export default function Calendar({ markedDates = {} }: CalendarProps) {
       <div className="flex items-center justify-between mb-6 mt-2">
         <button
           onClick={() => moveMonth(-1)}
-          className="bg-gray-700 rounded-lg w-9 h-9 text-xl cursor-pointer mr-3 hover:bg-gray-600 transition"
+          className="flex items-center justify-center bg-gray-700 rounded-lg w-9 h-9 cursor-pointer mr-3 hover:bg-gray-600 transition"
           aria-label="이전 달"
         >
-          {'<'}
+          <BackIcon className="stroke-gray-100" width={20} height={20} />
         </button>
         <span className="body-18-m text-gray-100 min-w-[120px] text-center">
           {monthName} {year}
         </span>
         <button
           onClick={() => moveMonth(1)}
-          className="bg-gray-700 rounded-lg w-9 h-9 text-xl cursor-pointer mr-3 hover:bg-gray-600 transition"
+          className="flex items-center justify-center bg-gray-700 rounded-lg w-9 h-9 cursor-pointer mr-3 hover:bg-gray-600 transition"
           aria-label="다음 달"
         >
-          {'>'}
+          <BackIcon
+            className="stroke-gray-100 rotate-180"
+            width={20}
+            height={20}
+          />
         </button>
       </div>
       <div className="grid grid-cols-7 my-2 gap-0.5">
