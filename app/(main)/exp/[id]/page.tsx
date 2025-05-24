bcbe95cc67f34d9f9763f56c80cfbe15b56e70fa
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-05-24 15:47:18 +0900

    feat/#98: 경험 삭제 구현

--- app/(main)/exp/[id]/page.tsx
+++ app/(main)/exp/[id]/page.tsx
@@ -4,11 +4,16 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import { ExpPayload, getExpById } from '@/apis/exp';
 import { EXP_OPTIONS } from '@/constants/expOptions';
+import { deleteExp } from '@/apis/exp';
+import { useRouter } from 'next/navigation';
+import Popup from '@/app/components/Popup';
 
 export default function ExpDetailPage() {
   const params = useParams();
-  const [data, setData] = useState<ExpPayload | null>(null);
+  const router = useRouter();
 
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [data, setData] = useState<ExpPayload | null>(null);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
@@ -102,7 +107,40 @@ export default function ExpDetailPage() {
 
   return (
     <main className="min-h-screen py-13 px-20 bg-black text-white">
-      <h1 className="text-3xl font-bold mb-[26px]">경험 상세</h1>
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold mb-[26px]">경험 상세</h1>
+        <div className="flex items-center gap-[9px]">
+          <button
+            type="button"
+            onClick={async () => {
+              setIsPopupOpen(true);
+            }}
+            className="w-20 py-3 bg-gray-1000 text-sm text-gray-300 font-semibold border border-gray-50-20 rounded-lg"
+          >
+            삭제
+          </button>
+          {isPopupOpen && (
+            <Popup
+              title="경험 삭제"
+              content={`경험을 삭제하시겠습니까?\n삭제하시면 다시 복구할 수 없습니다.`}
+              confirmText="삭제"
+              cancelText="취소"
+              onConfirm={async () => {
+                await deleteExp(Number(params?.id));
+                router.push('/exp');
+              }}
+              onCancel={() => setIsPopupOpen(false)}
+            />
+          )}
+          <button
+            type="button"
+            onClick={() => {}}
+            className="w-20 py-3 bg-primary-50 text-sm text-gray-1100 font-semibold rounded-lg"
+          >
+            수정
+          </button>
+        </div>
+      </div>
 
       <section className="bg-gray-800 p-8 rounded-[14px] text-gray-100">
         <h2 className="text-[21px] font-semibold mb-6 text-gray-50">내용</h2>
