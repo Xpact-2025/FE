e03852070655c888446f8b0494b0a728f5b6b9cf
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-07-04 19:52:39 +0900

    feat/#154: 북마크 추가

--- app/(main)/guide/components/AIActivityCard.tsx
+++ app/(main)/guide/components/AIActivityCard.tsx
@@ -2,6 +2,7 @@
 
 import { AIActivity } from '@/apis/guide';
 import BookmarkIcon from '@/public/icons/Bookmark.svg';
+import BookmarkFill from '@/public/icons/Bookmark_fill.svg';
 
 interface AIActivityProps {
   data: AIActivity;
@@ -13,7 +14,11 @@ export default function AIActivityCard({ data }: AIActivityProps) {
       <div className="w-48 h-48 rounded">이미지</div>
       <div className="flex items-center justify-between gap-27.5 pt-6">
         {data.scrapType}
-        <BookmarkIcon className="stroke-gray-100 w-[20px] h-[20px]" />
+        {data.isCliped ? (
+          <BookmarkIcon className="stroke-gray-100 w-[20px] h-[20px]" />
+        ) : (
+          <BookmarkFill className="stroke-gray-100 w-[20px] h-[20px]" />
+        )}
       </div>
       <div className="body-20-r pt-6">{data.title}</div>
       <div className="flex items-center justify-between body-16-r pt-2">
