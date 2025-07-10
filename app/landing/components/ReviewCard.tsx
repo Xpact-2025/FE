e2a2df65aac23244c46ccaeba1132f092bc6240e
heeone1 <rinakim1019@gmail.com>
2025-07-11 03:04:59 +0900

    fix: landing페이지 수정

--- app/landing/components/ReviewCard.tsx
+++ app/landing/components/ReviewCard.tsx
@@ -1,3 +1,44 @@
+// import Image from 'next/image';
+// import { useState } from 'react';
+
+// interface ReviewCardProps {
+//   text: string;
+//   name: string;
+//   school: string;
+// }
+
+// export default function ReviewCard({ text, name, school }: ReviewCardProps) {
+//   const [isHovered, setIsHovered] = useState(false);
+
+//   return (
+//     <div
+//       onMouseEnter={() => setIsHovered(true)}
+//       onMouseLeave={() => setIsHovered(false)}
+//       className={`w-full max-w-[373px] min-h-[300px] px-6 py-8 text-left shadow-lg transition hover:scale-[1.02] rounded-[8px] border-none outline-1 outline-white/10 ${
+//         isHovered ? 'bg-select-gradient' : 'bg-gray-900'
+//       }`}
+//     >
+//       <p className="text-[16px] leading-relaxed mb-6">{text}</p>
+//       <div className="flex items-center space-x-5">
+//         <div className="flex items-center justify-center overflow-hidden">
+//           <Image
+//             src="/images/reviewUser.svg"
+//             alt="user"
+//             width={44}
+//             height={44}
+//           />
+//         </div>
+//         <div className="text-[16px]">
+//           <div className="font-semibold">{name}</div>
+//           <div className="text-gray-400 text-sm">{school}</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+'use client';
+
 import Image from 'next/image';
 import { useState } from 'react';
 
@@ -9,24 +50,28 @@ interface ReviewCardProps {
 
 export default function ReviewCard({ text, name, school }: ReviewCardProps) {
   const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      className={`px-[30px] py-[37px] w-[373px] h-[272px] text-left shadow-lg hover:scale-[1.02] transition rounded-[8px] outline-1 outline-white/10 border-none ${isHovered ? 'bg-select-gradient' : 'bg-gray-900'}`}
+      className={`w-full max-w-[373px] h-[350px] px-6 py-8 text-left shadow-lg transition hover:scale-[1.02] rounded-[8px] border-none outline-1 outline-white/10 ${
+        isHovered ? 'bg-select-gradient' : 'bg-gray-900'
+      }`}
     >
       <p className="text-[16px] leading-relaxed mb-6">{text}</p>
-      <div className="flex items-center space-x-5">
-        <div className="w-[36px] h-[36px] rounded-full border border-white/20 flex items-center justify-center">
+
+      <div className="flex items-center space-x-5 absolute bottom-[5%] left-6">
+        <div className="flex items-center justify-center overflow-hidden">
           <Image
             src="/images/reviewUser.svg"
             alt="user"
-            width={36}
-            height={36}
+            width={44}
+            height={44}
           />
         </div>
         <div className="text-[16px]">
-          <div className="font-semibold">{name}</div>
+          <div className="font-semibold text-gray-400">{name}</div>
           <div className="text-gray-400 text-sm">{school}</div>
         </div>
       </div>
--- app/landing/components/Section1.tsx
+++ app/landing/components/Section1.tsx
@@ -1,17 +1,25 @@
+'use client';
+
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function Section1() {
+  const router = useRouter();
+
   return (
-    <section className="w-full max-w-[1440px] mx-auto flex flex-col items-center text-center px-[5%] py-[100px] relative isolate">
-      <div className="z-10 mt-[32px]">
+    <section className="flex flex-col items-center text-center py-[100px] relative isolate">
+      <div className="mx-auto z-10 mt-[32px]">
         <h2 className="text-[32px] md:text-[60px] text-gray-50 font-bold mb-4 leading-tight">
           한눈에 정리되는 <br /> 나의 경험
         </h2>
         <p className="text-[16px] md:text-[20px]">
-          경험 정리 대시보드, 직무 역량 분석, AI 자기소개서까지 <br />
+          경험 정리부터 직무 역량 분석, 맞춤 활동 추천까지 <br />
           취업 준비에 꼭 필요한 모든 과정을 한 번에 제공합니다.
         </p>
-        <button className="bg-white w-[120px] h-[40px] text-gray-1100 text-[16px] font-semibold px-6 py-2 rounded hover:bg-primary-50 active:bg-primary-100 transition mt-[45px]">
+        <button
+          className="bg-white w-[120px] h-[40px] text-gray-1100 text-[16px] font-semibold px-6 py-2 rounded hover:bg-primary-50 active:bg-primary-100 transition mt-[45px] cursor-pointer"
+          onClick={() => router.push('/login')}
+        >
           시작하기
         </button>
       </div>
@@ -22,7 +30,7 @@ export default function Section1() {
           width: '750px',
           height: '600px',
           top: '300px',
-          backgroundColor: '#6E1913',
+          backgroundColor: '#842800',
           filter: 'blur(200px)',
         }}
       />
@@ -32,18 +40,17 @@ export default function Section1() {
           width: '500px',
           height: '400px',
           top: '430px',
-          backgroundColor: '#E24B1A',
+          backgroundColor: '#FF6D01',
           filter: 'blur(100px)',
         }}
       />
-
-      <div className="relative z-10 mt-[67.88px] w-[1098px] h-[657px]">
+      <div className="relative z-10 mt-[10px] mx-auto justify-center items-center">
         <Image
-          src="/images/gray.svg"
+          src="/images/dashboard.png"
           alt="Xpact"
-          width={1098}
-          height={657}
-          className="rounded-[8px] w-full h-full object-cover"
+          width={1120}
+          height={815.25}
+          className="rounded-[8px] object-contain"
         />
         <div className="absolute bottom-0 left-0 w-full h-[150px] z-[11] bg-gradient-to-b from-black/0 to-[#050208]" />
       </div>
